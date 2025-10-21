d1b56335e0ac9c1dfd04e37bca72bcd0ac1b1019
sifat-samin <sifatsamin19@gmail.com>
2025-10-21 18:56:17 +0600

    NOT Found Page Added

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,6 +3,7 @@ import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 import { QueryClient } from '@tanstack/react-query'
 import Header from '@/components/Header'
+import { Link } from '@tanstack/react-router'
 
 type RouterContext = {
   queryClient: QueryClient
@@ -22,6 +23,7 @@ export const Route = createRootRouteWithContext<RouterContext>()({
   }),
 
   component: RootLayout,
+  notFoundComponent: NotFound,
 })
 
 function RootLayout() {
@@ -49,4 +51,22 @@ function RootLayout() {
       />
     </div>
   )
+}
+
+
+function NotFound() {
+  return (
+    <div className='flex flex-col items-center justify-center text-center py-20'>
+      <h1 className='text-4xl font-bold text-gray-800 mb-4'>404</h1>
+      <p className='text-lg text-gray-600 mb-6'>
+        Oops! The page you're looking for doesn't exist.
+      </p>
+      <Link
+        to='/'
+        className='px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition'
+      >
+        Go Back Home
+      </Link>
+    </div>
+  );
 }
