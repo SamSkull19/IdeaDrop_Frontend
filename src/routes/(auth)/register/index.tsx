d84970f2beb4fa107dc4f628a7318a2654eaa5fc
sifat-samin <sifatsamin19@gmail.com>
2025-10-23 17:36:50 +0600

    Register Connected With BAckend

--- src/routes/(auth)/register/index.tsx
+++ src/routes/(auth)/register/index.tsx
@@ -1,5 +1,8 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute, useNavigate, Link } from '@tanstack/react-router';
+import { useMutation } from '@tanstack/react-query';
 import { useState } from 'react';
+import { registerUser } from '@/api/auth';
+import { useAuth } from '@/context/AuthContext';
 
 export const Route = createFileRoute('/(auth)/register/')({
     component: RegisterPage,
@@ -9,15 +12,45 @@ function RegisterPage() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
-    const handleSubmit = (e: React.FormEvent) => {
+    const navigate = useNavigate();
+    const { setAccessToken, setUser } = useAuth();
+
+    const { mutateAsync, isPending } = useMutation({
+        mutationFn: registerUser,
+
+        onSuccess: (data) => {
+            setAccessToken(data.accessToken);
+            setUser(data.user);
+            navigate({ to: '/ideas' });
+        },
+
+        onError: (err: Error) => {
+            setError(err.message);
+        },
+    });
+
+    const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-        console.log('Registering user:', { name, email, password });
+        try {
+            await mutateAsync({ name, email, password });
+        }
+        catch (err: any) {
+            console.log(err.message);
+        }
     }
 
     return (
         <div className='max-w-md mx-auto'>
             <h1 className='text-3xl font-bold mb-6'>Register</h1>
+            {
+                error && (
+                    <div className='bg-red-100 text-red-700 px-4 py-2 rounded mb-4'>
+                        {error}
+                    </div>
+                )
+            }
             <form onSubmit={handleSubmit} className='space-y-4'>
                 <input
                     type='text'
@@ -45,9 +78,10 @@ function RegisterPage() {
                 />
                 <button
                     type='submit'
-                    className='bg-blue-600 hover:bg-blue-700 text-white font-semibold px-4 py-2 rounded-md w-full disabled:opacity-50'
+                    className='w-full bg-blue-600 hover:bg-blue-700 text-white py-2 rounded'
+                    disabled={isPending}
                 >
-                    Register
+                    {isPending ? 'Registering...' : 'Register'}
                 </button>
             </form>
 
