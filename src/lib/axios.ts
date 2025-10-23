b512a08950267b088df3eed72d530f8d755efbf0
sifat-samin <sifatsamin19@gmail.com>
2025-10-23 19:06:53 +0600

    Refresh Token When It Expires

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
-import { getStoredAccessToken } from '@/lib/authToken';
+import { getStoredAccessToken, setStoredAccessToken } from '@/lib/authToken';
+import { refreshAccessToken } from '@/api/auth';
 
 const api = axios.create({
     baseURL: '/api',
@@ -16,5 +17,28 @@ api.interceptors.request.use((config) => {
     }
     return config;
 });
+
+api.interceptors.response.use((res) => res,
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response?.status === 401 && !originalRequest._retry && !originalRequest.url.includes('/auth/refresh')) {
+
+            originalRequest._retry = true;
+
+            try {
+                const { accessToken: newToken } = await refreshAccessToken();
+                setStoredAccessToken(newToken);
+
+                originalRequest.headers.Authorization = `Bearer ${newToken}`;
+                return api(originalRequest);
+            }
+            catch (err) {
+                console.error('Refresh token failed', err);
+            }
+        }
+        return Promise.reject(error);
+    }
+);
 
 export default api;
